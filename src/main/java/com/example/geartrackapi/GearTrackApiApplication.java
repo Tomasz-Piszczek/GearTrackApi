1f2ec78199f7b75413e2d51f06610cf8b5e93e58
Tomasz <tomasz.piszczek@microsoft.wsei.edu.pl>
2025-07-13 16:20:02 +0200

    Initial commit

---
+++ src/main/java/com/example/geartrackapi/GearTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.geartrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearTrackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GearTrackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/geartrackapi/GearTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geartrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearTrackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
