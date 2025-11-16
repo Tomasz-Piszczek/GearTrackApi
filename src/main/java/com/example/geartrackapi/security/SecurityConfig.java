80130910d98dd31dbcd8fb20d338160d13733c66
Tomasz <tomasz.piszczek@microsoft.wsei.edu.pl>
2025-11-16 22:12:10 +0100

    allow health check

--- src/main/java/com/example/geartrackapi/security/SecurityConfig.java
+++ src/main/java/com/example/geartrackapi/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
